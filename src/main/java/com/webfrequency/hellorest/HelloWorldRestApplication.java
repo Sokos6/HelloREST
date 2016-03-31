5dca88a26d58b1c6ab2f9bf473029991f5e48317
Sokos6 <will@comicbooked.com>
2016-03-31 15:37:31 -0600

    first commit

---
+++ src/main/java/com/webfrequency/hellorest/HelloWorldRestApplication.java
@@ -0,0 +1,12 @@
+package com.webfrequency.hellorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldRestApplication.class, args);
+	}
+}
